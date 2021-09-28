86f3870da59d090b5afb5ddc2de37229b9b20dfc
DeronStantonjr <metagamejam@gmail.com>
2021-09-28 14:22:43 -0700

    ReadyPlayerMe

---
+++ Plugins/ReadyPlayerMe/Source/ReadyPlayerMe/Private/ReadyPlayerMeWebLink.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright © 2021++ Wolf3D
+
+#include "ReadyPlayerMeWebLink.h"
+
+
+void UReadyPlayerMeWebLink::UrlGenerated(FString url) {
+	UReadyPlayerMeWebLink::WebBrowserResponse.Execute(url);
+	lastAvatarUrl = url;
+}
+
+void UReadyPlayerMeWebLink::SetAvatarUrlCallback(FReadyPlayerWebBrowserResponse webBrowserCallback)
+{
+	UReadyPlayerMeWebLink::WebBrowserResponse = webBrowserCallback;
+}
+
+FString UReadyPlayerMeWebLink::GetLastAvatarUrl()
+{
+	return UReadyPlayerMeWebLink::lastAvatarUrl;
+}
