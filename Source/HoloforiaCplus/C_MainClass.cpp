d825ab02d105fc05dae0716e882300a59c1277ea
DeronStantonjr <metagamejam@gmail.com>
2021-09-28 16:58:59 -0700

    Ready Player Me SDK ( Configured )

---
+++ Source/HoloforiaCplus/C_MainClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_MainClass.h"
+
+C_MainClass::C_MainClass()
+{
+}
+
+C_MainClass::~C_MainClass()
+{
+}
